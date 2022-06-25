7fd7f2e1c08d321b813429a9c366ea60e01b48c9
sanandreasz <62306123+guwnos@users.noreply.github.com>
2022-06-25 21:22:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ Keyboard = Controller()
 
 time.sleep(3)
 for x in range(10): # Change 10 to amount messages to spam
-    for i in "Dupa": # Change Dupa for your message # Or if you want delete this line of code and change Keyboard.press(i) to Keyboard.type("message")
-        Keyboard.press(i)
-        Keyboard.press(Key.enter)
+    keyboard.type("Message to spam") # Change "Message to spam" to your message
+    keyboard.press(key.enter)
